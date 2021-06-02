fbd75ea0e2cf8ea6fa03eb33d56d795bf70d76fb
nuhashaikh <nuha.shaikh@gmail.com>
2021-06-02 18:04:05 +0000

    yellow_pager exercise

--- main.rb
+++ main.rb
@@ -18,41 +18,44 @@
 #     end
 # end
 
-def yellow_pager(stringOfLetters) 
-    if stringOfLetters.size!= 10
+
+def yellow_pager(stringOfLetters)
+    if stringOfLetters.size != 10
         return false   
     else
-        number_output=""
-        i=0
-        10.times do
-            if stringOfLetters(i) == 'A' ||stringOfLetters(i)== 'B' ||stringOfLetters(i)== 'C'
-                number_output+="2"
-                i++
-            elsif stringOfLetters(i) == 'D' ||stringOfLetters(i) == 'E' || stringOfLetters(i) =='F'
-                    number_output+="3"
-                    i++
-            elsif stringOfLetters(i) == 'G' ||stringOfLetters(i) == 'H' ||stringOfLetters(i) == 'I'
-                    number_output+="4"
-                    i++
-            elsif stringOfLetters(i) == 'J'|| stringOfLetters(i) =='K' || stringOfLetters(i)=='L'
-                    number_output+="5"
-                    i++
-            elsif stringOfLetters(i) == 'M' || stringOfLetters(i) =='N' ||stringOfLetters(i) == 'O'
-                    number_output+="6"
-                    i++
-            elsif stringOfLetters(i) == 'P' || stringOfLetters(i) =='Q' || stringOfLetters(i) =='R' || stringOfLetters(i) =='S'
-                    number_output+="7"
-                    i++
-            elsif stringOfLetters(i) == 'T' || stringOfLetters(i) =='U' || stringOfLetters(i) =='V'
-                    number_output+="8"
-                    i++
-            elsif stringOfLetters(i) == 'W' || stringOfLetters(i) =='X' || stringOfLetters(i) =='Y' || stringOfLetters(i) =='Z'
-                    number_output+="9"
-                    i++
-            end
-        end
-    end
+        number_output= decision_list(stringOfLetters)
         return number_output
+    end
 end
 
-p yellow_pager("helloworld")
+def decision_list(input_string)
+        input_string_length= input_string.size
+        number_output=""
+        i=0
+        input_string_length.times do
+                p input_string.slice(i)
+             if (input_string.slice(i) == 'A' ||input_string.slice(i)== 'B' ||input_string.slice(i)== 'C')
+                 number_output+="2"
+            elsif (input_string.slice(i) == 'D' ||input_string.slice(i) == 'E' || input_string.slice(i) =='F')
+                    number_output+="3"
+            elsif (input_string.slice(i) == 'G' ||input_string.slice(i) == 'H' ||input_string.slice(i) == 'I')
+                    number_output+="4"
+            elsif (input_string.slice(i) == 'J'|| input_string.slice(i) =='K' || input_string.slice(i)=='L')
+                    number_output+="5"
+            elsif (input_string.slice(i) == 'M' || input_string.slice(i) =='N' ||input_string.slice(i) == 'O')
+                    number_output+="6"
+            elsif (input_string.slice(i) == 'P' || input_string.slice(i) =='Q' || input_string.slice(i) =='R' || input_string.slice(i) =='S')
+                    number_output+="7"
+            elsif (input_string.slice(i) == 'T' || input_string.slice(i) =='U' || input_string.slice(i) =='V')
+                    number_output+="8"
+            elsif (input_string.slice(i) == 'W' || input_string.slice(i) =='X' || input_string.slice(i) =='Y' || input_string.slice(i) =='Z')
+                    number_output+="9"
+            else
+                number_output+=""
+             end
+             i+= 1
+         end
+     return number_output
+end
+
+p yellow_pager("HELLOWORLD")
