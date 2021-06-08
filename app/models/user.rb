20ef58621d18295611f15dbf7299f29e961917c1
nuhashaikh <nuha.shaikh@gmail.com>
2021-06-08 16:14:59 +0000

    W4D1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base # defining a class and it is inheriting from class ActiveRecord:: Base library
 
+    has_many :finstagram_post
+    has_many :comments
+    has_many :likes
+    
 end
