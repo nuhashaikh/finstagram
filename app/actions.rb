13024e494898cefc6c56c3cc48a814d9d229db7f
nuhashaikh <nuha.shaikh@gmail.com>
2021-06-09 23:27:45 +0000

    W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -6,5 +6,30 @@ get '/' do
         erb(:index) #this is calling the erb method, predefined
 
 end
+
+get '/signup' do # if a user navigates to the path "/signup"
+        @user= User.new # setup empty @user object
+        erb(:signup)   # render "app/views/signup.erb"
+end
+
+post '/signup' do
+        
+        # grab user input values from params
+        email = params[:email]
+        avatar_url= params[:avatar_url]
+        username= params[:username]
+        password= params[:password]
+
+        # instantiate and save a User
+        @user= User.new({email:email, avatar_url: avatar_url, username:username, password: password})
+
+        if @user.save
+                "User #{username} saved!"
+        else
+                # display error messages
+                erb(:signup)
+        end
+
+end
 # This code tells apps  to simply display the file 
 # called index.html from the directory app/views when the app is opened.
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base # defining a class and it is inheriting from cla
     has_many :finstagram_post
     has_many :comments
     has_many :likes
+
+    validates :email, :username, uniqueness: true
+    validates :email, :avatar_url, :username, :password, presence: true
     
 end
